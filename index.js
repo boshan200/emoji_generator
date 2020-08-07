55237397d1df98044d4b8b8a95745862b9a7f2c5
boshan200 <boshan200@gmail.com>
2020-08-07 16:00:37 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.export = (str)=>{
+    switch(str){
+        case "angry":
+            return "(#`Д´)ﾉ";
+        case "sad":
+            return "｡ﾟヽ(ﾟ´Д`)ﾉﾟ｡";
+        case "happy":
+            return "ヽ(́◕◞౪◟◕‵)ﾉ";
+    }
+};
